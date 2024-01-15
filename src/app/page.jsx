4604ea417ccaef53a088b11a67f505cb89807038
islamBelabbes <belabbesislam2@gmail.com>
2024-01-15 18:16:36 +0100

    Dashboard component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,31 +1,7 @@
-"use client";
-
-import SendMessageForm from "@/components/SendMessageForm/SendMessageForm";
-import { AnimatePresence } from "framer-motion";
-import Link from "next/link";
-import { useState } from "react";
+import Dashboard from "@/components/Dashboard/Dashboard";
 
 function page() {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <div className="app">
-      <AnimatePresence>
-        {isOpen && <SendMessageForm closeModal={() => setIsOpen(false)} />}
-      </AnimatePresence>
-
-      <div className="flex gap-3">
-        <div
-          className="cursor-pointer primary_button"
-          onClick={() => setIsOpen(true)}
-        >
-          Send Message
-        </div>
-        <Link className="cursor-pointer primary_button" href={"/dashboard"}>
-          Dashboard
-        </Link>
-      </div>
-    </div>
-  );
+  return <Dashboard />;
 }
 
 export default page;

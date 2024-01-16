a55d9ced42b7b24b9a59f91e9137620b7ef9382c
islamBelabbes <belabbesislam2@gmail.com>
2024-01-16 14:54:46 +0100

    add upstach ratelimit

--- src/app/api/message/route.js
+++ src/app/api/message/route.js
@@ -5,21 +5,22 @@ import {
   sendNoContent,
   sendOk,
   sendServerError,
+  sendToManyRequests,
 } from "@/lib/responseHelper";
 import { getMessages } from "@/lib/db";
 import { pusherServer } from "@/lib/pusher";
+import { ratelimit } from "@/lib/upStach";
 
 export const GET = async (req) => {
   const url = new URL(req.url);
   const limit = url.searchParams.get("limit");
   const page = url.searchParams.get("page");
+  const query = {
+    take: limit ? parseInt(limit) : 8,
+    skip: page ? parseInt(page) : 0,
+  };
 
-  const [data, error] = await tryCatch(
-    getMessages({
-      take: limit ? parseInt(limit) : 8,
-      skip: page ? parseInt(page) : 0,
-    })
-  );
+  const [data, error] = await tryCatch(getMessages(query));
 
   if (error) return sendServerError();
 
@@ -30,6 +31,11 @@ export const GET = async (req) => {
 };
 
 export const POST = async (req) => {
+  // rate limiter start
+  const { success } = await ratelimit.limit("ip");
+  if (!success) return sendToManyRequests();
+  // rate limiter end
+
   const { message, sender } = await req.json();
 
   const [data, error] = await tryCatch(
@@ -51,4 +57,23 @@ export const POST = async (req) => {
     data,
     message: "Message sent successfully",
   });
+};
+
+export const PUT = async (req) => {
+  const { ids } = await req.json();
+  const idsArray = ids.split(",").map((id) => ({ id }));
+
+  const [__, error] = await tryCatch(
+    prisma.message.updateMany({
+      where: {
+        AND: [...idsArray],
+      },
+      data: {
+        type: "Archive",
+      },
+    })
+  );
+
+  if (error) return sendServerError();
+  return sendNoContent();
 };

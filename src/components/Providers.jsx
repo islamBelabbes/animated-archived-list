24a07e945586745d6b0ad1bddbcda5f7e9675b2e
islamBelabbes <belabbesislam2@gmail.com>
2024-01-26 14:14:29 +0100

    fix csr-bailout

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -4,18 +4,21 @@ import { Next13ProgressBar } from "next13-progressbar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Suspense } from "react";
 
 const queryClient = new QueryClient();
 function Providers({ children }) {
   return (
     <QueryClientProvider client={queryClient}>
       <ToastContainer />
-      <Next13ProgressBar
-        height="4px"
-        color="#272343"
-        options={{ showSpinner: true }}
-      />
       {children}
+      <Suspense>
+        <Next13ProgressBar
+          height="4px"
+          color="#272343"
+          options={{ showSpinner: true }}
+        />
+      </Suspense>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
